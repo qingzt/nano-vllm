a3837e2381d9bb5a8a64eea5ebe0b83d8847ecfb
qingzt <754307254@qq.com>
2026-03-10 14:47:50 +0800

    update

--- nanovllm/models/qwen3.py
+++ nanovllm/models/qwen3.py
@@ -117,12 +117,15 @@ class Qwen3MLP(nn.Module):
 
 
 class Qwen3DecoderLayer(nn.Module):
-
     def __init__(
         self,
         config: Qwen3Config,
     ) -> None:
         super().__init__()
+        rope_scaling = getattr(config, "rope_scaling", None)
+        if isinstance(rope_scaling, dict) and rope_scaling.get("rope_type") == "default":
+            rope_scaling = None
+
         self.self_attn = Qwen3Attention(
             hidden_size=config.hidden_size,
             num_heads=config.num_attention_heads,
@@ -132,7 +135,7 @@ class Qwen3DecoderLayer(nn.Module):
             qkv_bias=getattr(config, 'attention_bias', True),
             head_dim=getattr(config, 'head_dim', None),
             rope_theta=getattr(config, "rope_theta", 1000000),
-            rope_scaling=getattr(config, "rope_scaling", None),
+            rope_scaling=rope_scaling,
         )
         self.mlp = Qwen3MLP(
             hidden_size=config.hidden_size,
